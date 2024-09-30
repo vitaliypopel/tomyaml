9667d12251660a3524ea620b86d66970af2baddc
vitaliypopel <vitaliypopel14@gmail.com>
2024-09-30 18:21:45 +0300

    Created abstract class for Serializer class

--- src/tomyaml/_abstractions.py
+++ src/tomyaml/_abstractions.py
@@ -1,6 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import Any, Callable, Dict, Literal, Optional, Union
 
+from formats import (
+    Format,
+    JSONFormat,
+    YAMLFormat,
+    TOMLFormat,
+    INIFormat,
+)
+
 
 class FormatAbstraction(ABC):
     __file_format: str
@@ -42,6 +50,59 @@ class FormatAbstraction(ABC):
     @property
     def string(self) -> str:
         return self.__string
+
+    @abstractmethod
+    def load(self, string: Union[str, bytes]) -> Optional[Dict[str, Any]]:
+        '''Loads a string or bytes into a dictionary'''
+        pass
+
+    @abstractmethod
+    def dump(self, dictionary: Dict[str, Any]) -> Optional[str]:
+        '''Dumps a dictionary into a string'''
+        pass
+
+    @abstractmethod
+    def to_dict(self) -> Dict[str, Any]:
+        '''Converts the current object to a dictionary'''
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        '''String representation of the object'''
+        pass
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        '''Representation of the object for debugging'''
+        pass
+
+
+class SerializerAbstraction(ABC):
+    __file_format: Format
+
+    def __init__(
+            self,
+            file_format: Literal['json', 'yaml', 'toml', 'ini'],
+    ):
+        file_format = file_format.lower()
+
+        if file_format == 'json':
+            self.__file_format = JSONFormat()
+        elif file_format == 'yaml':
+            self.__file_format = YAMLFormat()
+        elif file_format == 'toml':
+            self.__file_format = TOMLFormat()
+        elif file_format == 'ini':
+            self.__file_format = INIFormat()
+        else:
+            raise ValueError(
+                'Expected "json", "yaml", "toml" or "ini" file format, '
+                'instead got "%s"' % file_format.lower()
+            )
+
+    @property
+    def file_format(self) -> Format:
+        return self.__file_format
 
     @abstractmethod
     def load(self, string: Union[str, bytes]) -> Optional[Dict[str, Any]]:
