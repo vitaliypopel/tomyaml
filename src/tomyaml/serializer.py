9de2d464f010fa8463d2d2b444500e1827e1ec8b
vitaliypopel <vitaliypopel14@gmail.com>
2024-09-30 18:29:29 +0300

    Implemented Serializer class from abstraction

---
+++ src/tomyaml/serializer.py
@@ -0,0 +1,20 @@
+from typing import Union, Optional, Dict, Any
+
+from _abstractions import SerializerAbstraction
+
+
+class Serializer(SerializerAbstraction):
+    def load(self, string: Union[str, bytes]) -> Optional[Dict[str, Any]]:
+        return self.file_format.load(string)
+
+    def dump(self, dictionary: Dict[str, Any]) -> Optional[str]:
+        return self.file_format.dump(dictionary)
+
+    def to_dict(self) -> Dict[str, Any]:
+        return self.file_format.dictionary or {}
+
+    def __str__(self) -> str:
+        return self.file_format.string or ''
+
+    def __repr__(self) -> str:
+        return str(self)
